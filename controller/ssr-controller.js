a9b6c93431972a54b795c4a9df7abbe136010419
Lars <larsriel@gmail.com>
2022-02-25 15:07:00 +0100

    refactored to pre-render pages when the server starts instead of per request

--- controller/ssr-controller.js
+++ controller/ssr-controller.js
@@ -12,15 +12,27 @@ module.exports = {
     }
 }
 
-const home      = app => app.get('/',        (_, res) => res.send(renderPage('home')));
-const node      = app => app.get('/node',    (_, res) => res.send(renderPage('node')));
-const npm       = app => app.get('/npm',     (_, res) => res.send(renderPage('npm')));
-const express   = app => app.get('/express', (_, res) => res.send(renderPage('express')));
-const nodemon   = app => app.get('/nodemon', (_, res) => res.send(renderPage('nodemon')));
-const ssr       = app => app.get('/ssr',     (_, res) => res.send(renderPage('ssr')));
-const heroku    = app => app.get('/heroku',  (_, res) => res.send(renderPage('heroku')));
+// pre-render every page to store in memory
+// This way each page only has to be rendering once (when the server starts)
+// instead of every time the server receives a request
+const homePage    = renderPage('home');
+const nodePage    = renderPage('node');
+const npmPage     = renderPage('npm');
+const expressPage = renderPage('express');
+const nodemonPage = renderPage('nodemon');
+const ssrPage     = renderPage('ssr');
+const herokuPage  = renderPage('heroku');
 
-const renderPage = page => {
+// GET
+const home        = app => app.get('/',        (_, res) => res.send(homePage));    //  /home
+const node        = app => app.get('/node',    (_, res) => res.send(nodePage));    //  /node
+const npm         = app => app.get('/npm',     (_, res) => res.send(npmPage));     //  /npm
+const express     = app => app.get('/express', (_, res) => res.send(expressPage)); //  /express
+const nodemon     = app => app.get('/nodemon', (_, res) => res.send(nodemonPage)); //  /nodemon
+const ssr         = app => app.get('/ssr',     (_, res) => res.send(ssrPage));     //  /ssr
+const heroku      = app => app.get('/heroku',  (_, res) => res.send(herokuPage));  //  /heroku
+
+function renderPage(page) {
 
     const index = fs.readFileSync('./public/components/head/head.html').toString();
     const nav = fs.readFileSync('./public/components/nav/nav.html').toString();
