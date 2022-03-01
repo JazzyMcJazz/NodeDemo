f648f76f87853c4fd4e5f234076675e021f45004
Lars <larsriel@gmail.com>
2022-03-01 10:23:04 +0100

    stuff

--- 3._WWE/Lecture/app.js
+++ 3._WWE/Lecture/app.js
@@ -6,7 +6,7 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/views/index.html');
-})
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
