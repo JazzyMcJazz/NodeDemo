66285d28578c75ff198f38475add46b5454e539b
Lars <larsriel@gmail.com>
2022-04-05 16:20:28 +0200

    implemented UserRouter

---
+++ server/routers/UserRouter.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {getAllUsers} from "../repository/UserRepo.js";
+import {authenticate} from "../security/AuthConfig.js";
+
+const router = express.Router();
+
+// All endpoints are preceded by /api/users
+
+router.get('/', authenticate, async (req, res) => {
+    const user = req.user;
+    if (user.role === 'admin')
+        if (user.verified)
+            res.send(await getAllUsers());
+        else res.status(401).send({message: 'You must verify your account to access this information'})
+    else
+        res.status(401).send({message: 'Unauthorized. Admin privileges required'})
+});
+
+router.get('/self', authenticate, async (req, res) => {
+    res.send(req.user)
+});
+
+export default router;
